6c5f43f7805fc06163e88e08fcc35aeec93cb55d
OlhaPrykhodko <helga.prikhodko@gmail.com>
2026-06-17 14:56:47 +0300

    fix: [EL-5321] Import memo (#27)

--- frontend/src/pages/FeaturesPage/FeaturesPage.jsx
+++ frontend/src/pages/FeaturesPage/FeaturesPage.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, memo, useRef, useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
